d1c6286ae0750668cea607a0c8728b92a99cd3dd
Gupta <Swati.Gupta@capco.com>
2019-01-24 12:23:07 +0530

    second commit

--- visa-service/src/main/java/com/capco/travel/visa/controller/VisaRequestController.java
+++ visa-service/src/main/java/com/capco/travel/visa/controller/VisaRequestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.capco.travel.visa.model.VisaRequestBO;
 import com.capco.travel.visa.repository.VisaRepository;
 import com.capco.travel.visa.service.VisaService;
